8e6eb86f2bbba46536a215ec93238c8547044bb0
baekjung <zvgandam@gmail.com>
2019-11-12 14:30:11 +0900

    Conntect and Disconnect Notifications

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,5 +1,19 @@
 const notifications = document.getElementById("jsNotifications");
 
+const CONNECT_COLOR = "rgb(0, 122, 255)";
+const DISCONNECT_COLOR = "rgb(255, 59, 48)";
+
+const fireNotification = (text, color) => {
+  const notification = document.createElement("div");
+  notification.innerText = text;
+  notification.style.backgroundColor = color;
+  notifications.appendChild(notification);
+};
+
 export const handleNewUser = ({ nickname }) => {
-  console.log(nickname, " just joined");
+  fireNotification(`${nickname} just joined!`, CONNECT_COLOR);
+};
+
+export const handleDisconnected = ({ nickname }) => {
+  fireNotification(`${nickname} disconnected`, DISCONNECT_COLOR);
 };
